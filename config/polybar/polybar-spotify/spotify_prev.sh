6c00ad18d8a37cd290ed5512e0cbf41057ee4ecb
donotanswer <viktor.ferter@doclerholding.com>
2021-01-27 13:11:04 +0100

    polybar update

--- config/polybar/polybar-spotify/spotify_prev.sh
+++ config/polybar/polybar-spotify/spotify_prev.sh
@@ -28,23 +28,8 @@ PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -eq 0 ]; then
-    STATUS=$PLAYERCTL_STATUS
+    echo ""
 else
-    STATUS="No player is running"
+    echo ""
 fi
 
-if [ "$1" == "--status" ]; then
-    echo "$STATUS"
-else
-    if [ "$STATUS" = "Stopped" ]; then
-        echo "No music is playing"
-    elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
-        playerctl --player=$PLAYER metadata --format "$FORMAT"
-    elif [ "$STATUS" = "No player is running"  ]; then
-        echo ""
-    else
-        update_hooks "$PARENT_BAR_PID" 1
-        playerctl --player=$PLAYER metadata --format "$FORMAT"
-    fi
-fi
