60a900b28b6e7d212f86777ace8382878c2e7a6d
f-viktor <ferter.viktor@gmail.com>
2021-11-16 08:58:08 +0100

    Update yamanote.sh

--- config/polybar/yamanote/yamanote.sh
+++ config/polybar/yamanote/yamanote.sh
@@ -3,35 +3,39 @@ TAG=山手線
 # define interfaces
 WIFI=wlp3s0
 INTERNET=eno1
+PASSWORD=If_you_hav3_seen_this_1ts_probably_2_late
+CONNAME=hotsp
 
 enable_hotspot () {
-iw dev $WIFI interface add wlan_ap type managed addr 12:34:45:67:78:90
-ip addr add 192.168.99.1/24 dev wlan_ap
-systemctl start hostapd
-sysctl net.ipv4.ip_forward=1 -q
-iptables -t nat -A POSTROUTING -o $INTERNET -j MASQUERADE
-iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan_ap -o $INTERNET -j ACCEPT
-sysctl net/ipv4/ip_unprivileged_port_start=443 -q
-sysctl -w net.ipv4.conf.all.route_localnet=1 -q
-#systemctl start dnsmasq   #enable this if you dont have other dns providers
+	nmcli device wifi hotspot ifname $WIFI con-name $CONNAME ssid $TAG password $PASSWORD
+#iw dev $WIFI interface add wlan_ap type managed addr 12:34:45:67:78:90
+#ip addr add 192.168.99.1/24 dev wlan_ap
+#systemctl start hostapd
+#sysctl net.ipv4.ip_forward=1 -q
+#iptables -t nat -A POSTROUTING -o $INTERNET -j MASQUERADE
+#iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -i wlan_ap -o $INTERNET -j ACCEPT
+#sysctl net/ipv4/ip_unprivileged_port_start=80 -q
+#sysctl -w net.ipv4.conf.all.route_localnet=1 -q
+##systemctl start dnsmasq   #enable this if you dont have other dns providers
 }
 
 disable_hotspot() {
-iptables -t nat -F
-iptables -t mangle -F
-iptables -F
-iptables -X
-systemctl stop hostapd
-#systemctl stop dnsmasq
-sysctl net.ipv4.ip_forward=0 -q
-sysctl -w net.ipv4.conf.all.route_localnet=0 -q
-sysctl net/ipv4/ip_unprivileged_port_start=1024 -q
-iw dev wlan_ap del
+	nmcli c dowm hotsp
+#iptables -t nat -F
+#iptables -t mangle -F
+#iptables -F
+#iptables -X
+#systemctl stop hostapd
+##systemctl stop dnsmasq
+#sysctl net.ipv4.ip_forward=0 -q
+#sysctl -w net.ipv4.conf.all.route_localnet=0 -q
+#sysctl net/ipv4/ip_unprivileged_port_start=1024 -q
+#iw dev wlan_ap del
 }
 
 # decide whether the hotspot is working based on whether hostapd is running
-if systemctl status hostapd | grep -q "Active: active (running)"; then
+if nmcli con show --active | grep -q "$CONNAME"; then
 	if [[ $1 = "--toggle" ]]; then
 		disable_hotspot
 	fi
@@ -42,6 +46,8 @@ else
 	fi
 	echo $TAG
 fi
+
+
 
 
 
