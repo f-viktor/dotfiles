4805e5543090b728b85900386ccd41c10ee593c0
f-viktor <ferter.viktor@gmail.com>
2021-11-24 11:42:45 +0100

    it was about time to fix this anyway

--- config/polybar/audio-output/audio-output.sh
+++ config/polybar/audio-output/audio-output.sh
@@ -1,9 +1,14 @@
-HEADPHONE= 
+HEADPHONE=%{F\#ffb000}%{F-}
+CALLMODE=%{F\#ffb000}%{F-}
 SPEAKER=
-# pactl list sinks | grep "Description"  <-index numbmer for headphone output
+CURRENT_MODE="_"
+
+# pactl list sinks | grep "Description" <- pick your TWL headphone if you have one
 HEADPHONE_CARD_NAME="WH-1000XM3"
 
-
+#codecs to switch between for good audio vs bad audio+microphone
+GOOD_AUDIO="a2dp-sink-ldac"
+BAD_PLUS_MIC="headset-head-unit-msbc"
 
 function move_sinks_to_new_default {
     DEFAULT_SINK=$1
@@ -24,19 +29,40 @@ function set_default_playback_device_next {
     move_sinks_to_new_default $default_sink
 }
 
+function set_potato_hsf {
+    bluez_card_name=$(pactl list cards | grep $HEADPHONE_CARD_NAME -B 10 | grep "Name" | awk '{print $2}')
+    current_codec=$(pactl list sinks | grep "RUNNING" -A20 | grep "api.bluez5.profile")
+    if echo "$current_codec" | grep -q "a2dp"; then
+   	pactl set-card-profile $bluez_card_name $BAD_PLUS_MIC
+    else
+	pactl set-card-profile $bluez_card_name $GOOD_AUDIO
+    fi
+}
 
-if pactl list sinks | grep "RUNNING" -A 10| grep -q "$HEADPHONE_CARD_NAME"; then
-	if [[ $1 = "--toggle" ]]; then
-		echo $SPEAKER
-		set_default_playback_device_next
-	else
-		echo %{F\#ffb000}$HEADPHONE%{F-} 
-	fi
-else
-	if [[ $1 = "--toggle" ]]; then
-		echo %{F\#ffb000}$HEADPHONE%{F-} 
-		set_default_playback_device_next
-	else
-		echo $SPEAKER
-	fi
+#switch between audio outputs
+if [[ $1 = "--toggle" ]]; then
+	set_default_playback_device_next
 fi
+
+#change codecs of TWL device
+if [[ $1 = "--codec" ]]; then
+	set_potato_hsf  
+fi
+
+#print icon for current device 
+if pactl list sinks | grep "RUNNING" -A 10| grep -q "$HEADPHONE_CARD_NAME"; then
+    current_codec=$(pactl list sinks | grep "RUNNING" -A20 | grep "api.bluez5.profile")
+    if echo "$current_codec" | grep -q "a2dp"; then
+	CURRENT_MODE=$HEADPHONE
+    else
+	CURRENT_MODE=$CALLMODE
+    fi
+else
+	CURRENT_MODE=$SPEAKER
+fi
+
+echo $CURRENT_MODE 
+
+
+
+
