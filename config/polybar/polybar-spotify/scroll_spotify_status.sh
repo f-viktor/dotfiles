ad9b1b1e2541f6e1328ea2d9d4e59778cab0a856
f-viktor <ferter.viktor@gmail.com>
2022-03-17 21:48:54 +0100

    whoops

--- config/polybar/polybar-spotify/scroll_spotify_status.sh
+++ config/polybar/polybar-spotify/scroll_spotify_status.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
 # see man zscroll for documentation of the following parameters
+PLAYER=spotify
+
+PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ]; then
+
 zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding " ♪ " \
@@ -8,5 +16,8 @@ zscroll -l 30 \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true "$HOME/.config/polybar/polybar-spotify/get_spotify_status.sh" &
-
 wait
+
+else
+    echo ""
+fi
