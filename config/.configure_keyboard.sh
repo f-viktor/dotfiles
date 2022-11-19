c03ac9b96bd142b4b9f9c218053474d63d380048
f-viktor <ferter.viktor@gmail.com>
2022-11-19 14:16:44 +0100

    is this a keyboard?

--- config/.configure_keyboard.sh
+++ config/.configure_keyboard.sh
@@ -7,3 +7,7 @@ xset r rate 300 100 &
 xset m 1/1 0 &
 
 xmodmap /home/pepe/.Xmodmap
+
+
+#https://wiki.archlinux.org/title/Bluetooth#Wake_from_suspend
+# this also needs to happen if you want it on login: https://wiki.archlinux.org/title/Bluetooth#Discoverable_on_startup
